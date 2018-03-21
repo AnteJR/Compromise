ded2ba1be6864ce3bf5c96a56d4e3733bfd51234
Joël Rimaz <jo.ante2011@gmail.com>
2018-03-21 09:48:01 +0100

    Tests avec Meteor
    ça testouille

--- projet_g4/imports/ui/body1.js
+++ projet_g4/imports/ui/body1.js
@@ -3,3 +3,13 @@ import { Template } from 'meteor/templating';
 
 //importation des fichiers
 import './body.html';
+import './templates/template1.html';
+import { Semaines } from '../api/db_methods.js';
+
+//on dit bonjour
+Template.bonjour1.helpers({
+	texte: function(){
+		monTxt = "Salut";
+		return monTxt;
+	}
+});
