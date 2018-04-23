5c792664689a5b12ab5fe0f06d233a7d8d2e1ca6
Joël <jo.ante2011@gmail.com>
2018-04-23 14:32:25 +0200

    Solution à la truelle pour l'attribution des values au tableau
    A voir pour optimiser et pour afficher les bonnes couleurs

--- projet_g4/imports/ui/connexion.js
+++ projet_g4/imports/ui/connexion.js
@@ -32,34 +32,192 @@ Accounts.onLogin(function(user){
 	}
 	let mesScores = scoresUtilisateurCourant(Meteor.userId());
     //lancement de la fonction de création de tableaux
-    creerTableau(mesScores, mesJours)
+    creerTableau(mesScores);
+    //Helpers pour les tableaux
+	Template.tableauSemaines.helpers({
+		jour: [
+			{nomJour: "lundi"},
+			{nomJour: "mardi"},
+			{nomJour: "mercredi"},
+			{nomJour: "jeudi"},
+			{nomJour: "vendredi"},
+			{nomJour: "samedi"},
+			{nomJour: "dimanche"},
+		],
+	});
 });
 
 //fonction pour créer un tableau
-function creerTableau(scores, jours){
-	//test de l'envoi de données
-	console.log(scores);
-	//création d'un élément tableau, ajout d'un id
-	let monTab = document.createElement("table");
-	monTab.setAttribute("id", "semaine");
-	document.body.appendChild(monTab);
-	//tentative d'avoir un truc pour supprimer les éléments du tableau
-	/*let monTabEmptier = document.getElementById("semaine");
-	while(monTabEmptier.hasChildNodes()){monTabEmptier.removeChild(firstChild())}*/
-	//système de deux boucles pour peupler le tableau
-	for(let i=0;i<scores.length;i++){
-		let monTr = document.createElement("tr");
-		monTab.appendChild(monTr);
-		for(let j=0;j<scores[i].length;j++){
-			let monTd = document.createElement("td");
-			//on ajoute aux td un id suivant le modèle "jour_heure"
-			monTd.setAttribute("id", jours[i]+"_"+j);
-			//on affiche son score
-			monTd.value = scores[i][j];
-			monTd.innerHTML = monTd.value;
-			monTr.appendChild(monTd);
-		}
-	}
+function creerTableau(mesScores){
+	Template.newTd.helpers({
+		periode: [
+			{
+				heure: "8:00", 
+				id_heure: 0, 
+				valeurLundi: mesScores[0][0],
+				valeurMardi: mesScores[1][0],
+				valeurMercredi: mesScores[2][0],
+				valeurJeudi: mesScores[3][0],
+				valeurVendredi: mesScores[4][0],
+				valeurSamedi: mesScores[5][0],
+				valeurDimanche: mesScores[6][0]
+			},
+			{
+				heure: "9:00", 
+				id_heure: 1,
+				valeurLundi: mesScores[0][1],
+				valeurMardi: mesScores[1][1],
+				valeurMercredi: mesScores[2][1],
+				valeurJeudi: mesScores[3][1],
+				valeurVendredi: mesScores[4][1],
+				valeurSamedi: mesScores[5][1],
+				valeurDimanche: mesScores[6][1]
+			},
+			{
+				heure: "10:00", 
+				id_heure: 2,
+				valeurLundi: mesScores[0][2],
+				valeurMardi: mesScores[1][2],
+				valeurMercredi: mesScores[2][2],
+				valeurJeudi: mesScores[3][2],
+				valeurVendredi: mesScores[4][2],
+				valeurSamedi: mesScores[5][2],
+				valeurDimanche: mesScores[6][2]
+			},
+			{
+				heure: "11:00", 
+				id_heure: 3,
+				valeurLundi: mesScores[0][3],
+				valeurMardi: mesScores[1][3],
+				valeurMercredi: mesScores[2][3],
+				valeurJeudi: mesScores[3][3],
+				valeurVendredi: mesScores[4][3],
+				valeurSamedi: mesScores[5][3],
+				valeurDimanche: mesScores[6][3]
+			},
+			{
+				heure: "12:00", 
+				id_heure: 4,
+				valeurLundi: mesScores[0][4],
+				valeurMardi: mesScores[1][4],
+				valeurMercredi: mesScores[2][4],
+				valeurJeudi: mesScores[3][4],
+				valeurVendredi: mesScores[4][4],
+				valeurSamedi: mesScores[5][4],
+				valeurDimanche: mesScores[6][4]
+			},
+			{
+				heure: "13:00", 
+				id_heure: 5,
+				valeurLundi: mesScores[0][5],
+				valeurMardi: mesScores[1][5],
+				valeurMercredi: mesScores[2][5],
+				valeurJeudi: mesScores[3][5],
+				valeurVendredi: mesScores[4][5],
+				valeurSamedi: mesScores[5][5],
+				valeurDimanche: mesScores[6][5]
+			},
+			{
+				heure: "14:00", 
+				id_heure: 6,
+				valeurLundi: mesScores[0][6],
+				valeurMardi: mesScores[1][6],
+				valeurMercredi: mesScores[2][6],
+				valeurJeudi: mesScores[3][6],
+				valeurVendredi: mesScores[4][6],
+				valeurSamedi: mesScores[5][6],
+				valeurDimanche: mesScores[6][6]
+			},
+			{
+				heure: "15:00", 
+				id_heure: 7,
+				valeurLundi: mesScores[0][7],
+				valeurMardi: mesScores[1][7],
+				valeurMercredi: mesScores[2][7],
+				valeurJeudi: mesScores[3][7],
+				valeurVendredi: mesScores[4][7],
+				valeurSamedi: mesScores[5][7],
+				valeurDimanche: mesScores[6][7]
+			},
+			{
+				heure: "16:00", 
+				id_heure: 8,
+				valeurLundi: mesScores[0][8],
+				valeurMardi: mesScores[1][8],
+				valeurMercredi: mesScores[2][8],
+				valeurJeudi: mesScores[3][8],
+				valeurVendredi: mesScores[4][8],
+				valeurSamedi: mesScores[5][8],
+				valeurDimanche: mesScores[6][8]
+			},
+			{
+				heure: "17:00", 
+				id_heure: 9,
+				valeurLundi: mesScores[0][9],
+				valeurMardi: mesScores[1][9],
+				valeurMercredi: mesScores[2][9],
+				valeurJeudi: mesScores[3][9],
+				valeurVendredi: mesScores[4][9],
+				valeurSamedi: mesScores[5][9],
+				valeurDimanche: mesScores[6][9]
+			},
+			{
+				heure: "18:00", 
+				id_heure: 10,
+				valeurLundi: mesScores[0][10],
+				valeurMardi: mesScores[1][10],
+				valeurMercredi: mesScores[2][10],
+				valeurJeudi: mesScores[3][10],
+				valeurVendredi: mesScores[4][10],
+				valeurSamedi: mesScores[5][10],
+				valeurDimanche: mesScores[6][10]
+			},
+			{
+				heure: "19:00", 
+				id_heure: 11,
+				valeurLundi: mesScores[0][11],
+				valeurMardi: mesScores[1][11],
+				valeurMercredi: mesScores[2][11],
+				valeurJeudi: mesScores[3][11],
+				valeurVendredi: mesScores[4][11],
+				valeurSamedi: mesScores[5][11],
+				valeurDimanche: mesScores[6][11]
+			},
+			{
+				heure: "20:00", 
+				id_heure: 12,
+				valeurLundi: mesScores[0][12],
+				valeurMardi: mesScores[1][12],
+				valeurMercredi: mesScores[2][12],
+				valeurJeudi: mesScores[3][12],
+				valeurVendredi: mesScores[4][12],
+				valeurSamedi: mesScores[5][12],
+				valeurDimanche: mesScores[6][12]
+			},
+			{
+				heure: "21:00", 
+				id_heure: 13,
+				valeurLundi: mesScores[0][13],
+				valeurMardi: mesScores[1][13],
+				valeurMercredi: mesScores[2][13],
+				valeurJeudi: mesScores[3][13],
+				valeurVendredi: mesScores[4][13],
+				valeurSamedi: mesScores[5][13],
+				valeurDimanche: mesScores[6][13]
+			},
+			{
+				heure: "22:00", 
+				id_heure: 14,
+				valeurLundi: mesScores[0][14],
+				valeurMardi: mesScores[1][14],
+				valeurMercredi: mesScores[2][14],
+				valeurJeudi: mesScores[3][14],
+				valeurVendredi: mesScores[4][14],
+				valeurSamedi: mesScores[5][14],
+				valeurDimanche: mesScores[6][14]
+			},
+		],
+	});
 }
 
 //Fonction qui retourne au tableau contenant les disponibilités d'un utilisateur donné
@@ -92,7 +250,7 @@ Template.login.events({
 		console.log(Meteor.userId()+" "+jour+" "+heure+" "+elemVal);
 		//appel de la méthode semaines.updateTable
 		//à voir si l'id de la semaine n'est pas plus pertinent. Cependant il faut voir où le stocker pour le vérifier dans la méthode
-		Meteor.call("semaines.updateTable", Meteor.userId(), jour, heure, elemVal);
+		if(!isNaN(elemVal)){Meteor.call("semaines.updateTable", Meteor.userId(), jour, heure, elemVal)};
 	},
 	'click .pressMe': function(event){
 		event.preventDefault();
@@ -111,54 +269,8 @@ Template.login.events({
     		mesScores3.push(placeHolder);
     	}
     	//lancement de la fonction de création de tableau avec, en donnée, l'array compilant les deux tableaux
-    	creerTableau(mesScores3, mesJours);
+    	creerTableau(mesScores3);
 	}
-});
-
-//Helpers pour les tableaux
-Template.tableauSemaines.helpers({
-	jour: [
-		{nomJour: "lundi"},
-		{nomJour: "mardi"},
-		{nomJour: "mercredi"},
-		{nomJour: "jeudi"},
-		{nomJour: "vendredi"},
-		{nomJour: "samedi"},
-		{nomJour: "dimanche"},
-	],
-});
-
-Template.newTr.helpers({
-	jour: [
-		{nbreJour:1},
-		{nbreJour:2},
-		{nbreJour:3},
-		{nbreJour:4},
-		{nbreJour:5},
-		{nbreJour:6},
-		{nbreJour:7},
-		{nbreJour:8},
-	],
-});
-
-Template.newTd.helpers({
-	periode: [
-		{heure: "8:00", id_heure: 0},
-		{heure: "9:00", id_heure: 1},
-		{heure: "10:00", id_heure: 2},
-		{heure: "11:00", id_heure: 3},
-		{heure: "12:00", id_heure: 4},
-		{heure: "13:00", id_heure: 5},
-		{heure: "14:00", id_heure: 6},
-		{heure: "15:00", id_heure: 7},
-		{heure: "16:00", id_heure: 8},
-		{heure: "17:00", id_heure: 9},
-		{heure: "18:00", id_heure: 10},
-		{heure: "19:00", id_heure: 11},
-		{heure: "20:00", id_heure: 12},
-		{heure: "21:00", id_heure: 13},
-		{heure: "22:00", id_heure: 14},
-	],
 });
 
 var valeur;
