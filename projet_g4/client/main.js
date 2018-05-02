d4561a435c5096514d007192d6890d302149d7b4
Sorcha Walsh <33891407+sorchawalsh@users.noreply.github.com>
2018-05-02 08:35:25 +0200

    Merge branch 'master' into sorchatest2

--- projet_g4/client/main.js
+++ projet_g4/client/main.js
@@ -1,2 +1,6 @@
 import '../imports/ui/connexion.js';
 //import '../imports/ui/editSemaine.js';
+import { Meteor } from 'meteor/meteor'
+import { Mongo } from 'meteor/mongo'
+Meteor.subscribe('semaines');
+Meteor.subscribe('users');
--- projet_g4/server/main.js
+++ projet_g4/server/main.js
@@ -3,4 +3,17 @@ import '../imports/api/semaines.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Meteor.publish('semaines',function () {
+    return semaines.find({isPrivate:false})
+  })
+
+Meteor.publish('users', function () {
+//publish some of users' information to client
+return Meteor.users.find({},
+{
+  //specify information to publish from the users' collection
+    fields:{ 'emails': 1,
+     '_id':1,}
+})
+})
 });
