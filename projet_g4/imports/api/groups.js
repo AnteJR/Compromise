cf7f8fc9cac70d75f1c9fcc125b515fb3c28e774
Sorcha Walsh <33891407+sorchawalsh@users.noreply.github.com>
2018-05-09 09:48:17 +0200

    Add flow-router, add groups

---
+++ projet_g4/imports/api/groups.js
@@ -0,0 +1,20 @@
+//Import des méthodes
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
+import { Semaines } from '../api/semaines.js';
+
+export const Groups = new Mongo.Collection('groups');
+
+//méthodes
+
+Meteor.methods({
+    'groups.create'(idUt){
+    check(idUt, String);
+    Groups.insert({
+        'admin':idUt,
+        'isCreated': true,
+        'name': 'Mon groupe',
+    })
+    }
+})
